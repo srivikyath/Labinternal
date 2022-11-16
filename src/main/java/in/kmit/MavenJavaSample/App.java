326a8a3cc7524e717df4c1d193dd801716373b95
srivikyath <srivikyath@gmail.com>
2022-11-16 11:47:04 +0530

    commited

---
+++ src/main/java/in/kmit/MavenJavaSample/App.java
@@ -0,0 +1,13 @@
+package in.kmit.MavenJavaSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam,The created project is MavenJavaSample" );
+    }
+}
